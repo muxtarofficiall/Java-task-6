b1be68354717d42148f8a34c0423e671c1649df9
Mukhtar Mammadov <161587396+muxtarofficiall@users.noreply.github.com>
2024-04-03 00:13:00 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+///1.mehsullarin ucuzdan bahaya siralanmasi (TASK 1)
+
+//const products = [
+//{name:"Milk", price:14},
+//{name:"jUICE TEA", price:13},
+//{name:"tvorog", price:20}
+// ];
+//products.sort(function(a, b){return a.price - b.price});
+//console.log(products)
+
+
+///2.Beş random eded yaradib arraya yigin, sonda ise onlarin cemini gosterin (TASK 2)
+
+//let x = Math.floor((Math.random() * 5) + 1);
+//let y = Math.floor((Math.random() * 5) + 1);
+//let z = Math.floor((Math.random() * 5) + 1);
+//let a = Math.floor((Math.random() * 5) + 1);
+//let n = Math.floor((Math.random() * 5) + 1);
+//console.log(x + y + z + a + n)
+
+
+
